d1850bc7d0ac687547d9e2ceedf4e9b7574d7f08
Anthony <anthony-hoens@hotmail.com>
2020-04-27 18:10:56 +0200

    Form updated

--- controllers/team.php
+++ controllers/team.php
@@ -7,17 +7,35 @@ require('./models/team.php');
 
 function store(\PDO $pdo)
 {
+    if(!isset($_POST['name']) || trim($_POST['name'])==='') {
+        $_SESSION['errors']['name'] = 'Vous devez entrer un nom pour une équipe';
+    }
+    if(!isset($_POST['slug']) || trim($_POST['slug'])==='') {
+        $_SESSION['errors']['slug'] = 'Vous devez entrer un slug pour une équipe';
+    } elseif(strlen($_POST['slug'])!=3) {
+        $_SESSION['errors']['slug'] = 'Vous devez entrer un slug de 3 caractères exactement';
+
+    }
+
     $name = $_POST['name'];
     $slug = $_POST['slug'];
 
-    save($pdo, compact('name', 'slug'));
+    if(!$_SESSION['errors']) {
+        save($pdo, compact('name', 'slug'));
+        header('Location: index.php');
+        exit();
+    }
 
-    header('Location: index.php');
+    $_SESSION['old']['name'] = $_POST['name'];
+    $_SESSION['old']['slug'] = $_POST['slug'];
+
+    header('Location: index.php?action=create&resource=team');
     exit();
 }
 
 function create()
 {
     $view = './views/team/create.php';
+
     return compact('view');
 }
